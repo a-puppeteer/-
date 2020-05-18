c00cb38d5d89d839d07ad43fc6a51600b68c1b11
jw <408760565@qq.com>
2020-05-19 03:51:58 +0800

    babel和class

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,6 +3,28 @@ var $ = require( "jquery" );
 // console.log($);
 import './css/index.css'
 // import './css/index.less'
-console.log($('.t'));
-console.log('23663');
+// console.log($('.t'));
+// console.log('23663');
 
+class father {
+  constructor(x,y){
+    this.x = x
+    this.y = y
+  }
+  sum(){
+    console.log(this.x+this.y);
+    
+  }
+}
+ class son extends father{
+  constructor(x,y){
+    // this.x = x
+    // this.y = y
+    super(x,y)
+  }
+ }
+// var jw = new father(5,6)
+var jwson = new son(8,9)
+// jw.sum()
+jwson.sum()
+// console.log(jwson,11);
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       {test:/\.less$/,use:['style-loader','css-loader','postcss-loader','less-loader']},
       {test:/\.scss$/,use:['style-loader','css-loader','postcss-loader','sass-loader']},
       {test:/\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/,use:['url-loader?limit=16940']},
+      {test:/\.js$/,use:['babel-loader'],exclude:/node_modules/},
     ]
   }
 }
