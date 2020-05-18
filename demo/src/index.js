2a8a4429fd712706f977a198a235b525b281e6e1
jw <408760565@qq.com>
2020-05-18 20:17:41 +0800

    打包图片和css兼容

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,8 @@
 var $ = require( "jquery" );
 // import $ from "jquery";
 // console.log($);
-import './index.css'
+import './css/index.css'
+// import './css/index.less'
 console.log($('.t'));
 console.log('23663');
 
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -9,9 +9,10 @@ module.exports = {
   plugins:[htmlPlugin],
   module:{
     rules:[
-      {test:/\.css$/,use:['style-loader','css-loader']},
-      {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
-      {test:/\.scss$/,use:['style-loader','css-loader','sass-loader']},
+      {test:/\.css$/,use:['style-loader','css-loader','postcss-loader']},
+      {test:/\.less$/,use:['style-loader','css-loader','postcss-loader','less-loader']},
+      {test:/\.scss$/,use:['style-loader','css-loader','postcss-loader','sass-loader']},
+      {test:/\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/,use:['url-loader?limit=16940']},
     ]
   }
 }
